7bd5345a98c68a1c929ee08949b4037fc23b5adf
OngarbaevI <71922313+OngarbaevI@users.noreply.github.com>
2020-12-05 16:38:19 +0700

    Update Lab_4.c

--- Lab_4.c
+++ Lab_4.c
@@ -8,13 +8,14 @@
 
 
 void main(int argc, char* argv[]) {
+    //задаем имя симофоров асинхронного симафора
+    char *semOneName = "/sem1"; 
+    char *semTwoName = "/sem2";
+    //создаем асинхронные симафоры 
+    sem_t *sem1 = sem_open(semOneName, O_CREAT | O_EXCL, O_RDWR, 0); //!
+    sem_t *sem2 = sem_open(semTwoName, O_CREAT | O_EXCL, O_RDWR, 1); //!
 
-    char *semOneName = "/mysem1";
-    char *semTwoName = "/mysem2";
-    sem_t *sem1 = sem_open(semOneName, O_CREAT | O_EXCL, O_RDWR, 0);
-    sem_t *sem2 = sem_open(semTwoName, O_CREAT | O_EXCL, O_RDWR, 1);
-
-    if(fork())
+    if(fork())//создание подроцессы
     {
         for(int i = 0; i < 3; i++){
             sem_wait(sem1);
@@ -27,12 +28,13 @@ void main(int argc, char* argv[]) {
     }
     else {
         for(int i = 0; i < 3; i++){
-            sem_wait(sem2);
+            sem_wait(sem2); //!
             FILE *filelog = fopen("log.txt", "a");
-            fprintf(filelog, "%d <-- \n", getpid());
-            fclose(filelog);
-            sem_post(sem1);
+            fprintf(filelog, "%d <-- \n", getpid()); //
+            fclose(filelog); // работа с логами
+            sem_post(sem1);//!
         }
     }
+    //обязательно для того чтобы не тратить память
     sem_unlink(semOneName);
     sem_unlink(semTwoName);
