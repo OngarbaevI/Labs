3179a5bbc280cd5af6d5283b2adf800c86363ccc
OngarbaevI <71922313+OngarbaevI@users.noreply.github.com>
2020-12-05 16:56:24 +0700

    Update Lab_4.c

--- Lab_4.c
+++ Lab_4.c
@@ -8,31 +8,32 @@
 
 
 void main(int argc, char* argv[]) {
-    //задаем имя симофоров асинхронного симафора
+    //задаем имя асинхронного симофоров
     char *semOneName = "/sem1"; 
     char *semTwoName = "/sem2";
     //создаем асинхронные симафоры 
     sem_t *sem1 = sem_open(semOneName, O_CREAT | O_EXCL, O_RDWR, 0); //!
     sem_t *sem2 = sem_open(semTwoName, O_CREAT | O_EXCL, O_RDWR, 1); //!
 
-    if(fork())//создание подроцессы
+    if(fork())//создание подроцесса
     {
         for(int i = 0; i < 3; i++){
-            sem_wait(sem1);
-            FILE *filelog = fopen("log.txt", "a");
-            fprintf(filelog, "%d <--- \n", getpid());
-            fclose(filelog);
-            sem_post(sem2);
+            sem_wait(sem1);                             //уменьшает значение семафора на единицу и блокирует выполнение любых потоков
+            FILE *filelog = fopen("log.txt", "a");      //открываем файл логов
+            fprintf(filelog, "%d <--- \n", getpid());   //записывам логи
+            fclose(filelog);                            //закрывааем файл 
+            sem_post(sem2);                             //увеличивает значение семафора на единицу и возобновляет выполнение любых потоков
+        }
 
         }
     }
     else {
         for(int i = 0; i < 3; i++){
-            sem_wait(sem2); //!
-            FILE *filelog = fopen("log.txt", "a");
-            fprintf(filelog, "%d <-- \n", getpid()); //
-            fclose(filelog); // работа с логами
-            sem_post(sem1);//!
+            sem_wait(sem2);                             //уменьшает значение семафора на единицу и блокирует выполнение любых потоков
+            FILE *filelog = fopen("log.txt", "a");      //открываем файл логов
+            fprintf(filelog, "%d <-- \n", getpid());    //записывам логи
+            fclose(filelog);                            //закрывааем файл
+            sem_post(sem1);                             //увеличивает значение семафора на единицу и возобновляет выполнение любых потоков
         }
     }
     //обязательно для того чтобы не тратить память
